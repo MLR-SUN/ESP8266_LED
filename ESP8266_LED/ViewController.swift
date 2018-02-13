9e2db3da6be2bb5df727c6793aad78ce7de1efe2
M.L.R <sun.ios06444@gmail.com>
2018-02-13 20:49:35 +0800

    revise view

--- ESP8266_LED/ViewController.swift
+++ ESP8266_LED/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var web: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,17 +22,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func on(_ sender: Any) {
-        let url = URL(string: "http://172.20.10.3/gpio/1")
-        let urlreq = NSURLRequest(url: url! as URL)
-        web.loadRequest(urlreq as URLRequest)
+
+   
+    
+    @IBAction func `switch`(_ sender: UISwitch) {
+
+        
+        if (sender.isOn == true){
+            
+            
+            let url = URL(string: "http://172.20.10.3/gpio/1")
+            let urlreq = NSURLRequest(url: url! as URL)
+            web.loadRequest(urlreq as URLRequest)
+            
+            // create the alert
+            let alert = UIAlertController(title: "Notice", message: "LED ON", preferredStyle: UIAlertControllerStyle.alert)
+            
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            
+        }else{
+            let url = URL(string: "http://172.20.10.3/gpio/0")
+            let urlreq = NSURLRequest(url: url! as URL)
+            web.loadRequest(urlreq as URLRequest)
+                
+            
+        
+        }
     }
     
-
-    @IBAction func off(_ sender: Any) {
-        let url = URL(string: "http://172.20.10.3/gpio/0")
-        let urlreq = NSURLRequest(url: url! as URL)
-        web.loadRequest(urlreq as URLRequest)
-    }
+    
 }
 
